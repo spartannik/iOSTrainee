2fe96fa515d29206bd6209dcd86eb37f0b319045
spartannik <dead5042@gmail.com>
2022-09-22 16:46:33 +0300

    создаю контроллеры

---
+++ iOSTrainee/ViewControllers/DetailsViewController.swift
@@ -0,0 +1,157 @@
+//
+//  DetailsViewController.swift
+//  iOSTrainee
+//
+//  Created by Nikita Yashchenko on 22.09.2022.
+//
+
+import UIKit
+
+final class DetailsViewController: UIViewController {
+    
+    private var networkManager = NetworkManager()
+    var singlePostUrl: URL?
+    
+    //MARK: - UI
+    private let activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        return activityIndicator
+    }()
+    
+    private let scrollView = UIScrollView()
+    
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.backgroundColor = .blue
+        contentView.frame.size = CGSize(width: scrollView.contentSize.width, height: scrollView.contentSize.height)
+        return contentView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //MARK: - ViewLifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configureNavigationBar()
+        configureScrollView()
+        layout()
+        activityIndicator.startAnimating()
+        
+        networkManager.fetchData(url: singlePostUrl, type: SinglePostModel.self) { singlePost in
+            DispatchQueue.main.async { [weak self] in
+                self?.updateDetailsViewController(with: singlePost)
+                self?.activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    //MARK: - NavigationBar
+    private func configureNavigationBar() {
+        navigationItem.title = "Natife"
+        navigationController?.navigationBar.tintColor = #colorLiteral(red: 0.2274509804, green: 0.3254901961, blue: 0.3647058824, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20) ,
+            NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.2274509804, green: 0.3254901961, blue: 0.3647058824, alpha: 1)]
+    }
+    
+    //MARK: - ScrollView
+    private func configureScrollView() {
+        scrollView.backgroundColor = .white
+        scrollView.frame = view.frame
+        scrollView.contentSize.height = contentView.bounds.height
+        scrollView.contentSize.width = view.bounds.width
+    }
+    
+    private func updateDetailsViewController(with p: SinglePostModel) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMMM yyyy"
+        let timeInterval = Date(timeIntervalSince1970: TimeInterval(p.post.timeshamp))
+        let date = dateFormatter.string(from: timeInterval)
+        
+        guard let imageUrl = URL(string: p.post.postImage) else { return }
+        guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+        
+        titleLabel.text = p.post.title
+        descriptionLabel.text = p.post.text
+        likesLabel.text = "❤️ \(p.post.likesCount)"
+        dateLabel.text = date
+        imageView.image = UIImage(data: imageData)
+        
+    }
+    
+    //MARK: - Layout
+    private func layout() {
+        view.addSubview(scrollView)
+        view.addSubview(activityIndicator)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(likesLabel)
+        contentView.addSubview(dateLabel)
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 300),
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            
+            likesLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 20),
+            likesLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            likesLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            dateLabel.topAnchor.constraint(equalTo: likesLabel.topAnchor),
+            dateLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            dateLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            dateLabel.heightAnchor.constraint(equalToConstant: 20),
+        ])
+    }
+}
---
+++ iOSTrainee/ViewControllers/MainViewController.swift
@@ -0,0 +1,137 @@
+//
+//  MainViewController.swift
+//  iOSTrainee
+//
+//  Created by Nikita Yashchenko on 22.09.2022.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    private var networkManager = NetworkManager()
+    private var url = URL(string: "https://raw.githubusercontent.com/anton-natife/jsons/master/api/main.json")
+    private var posts: [Post]?
+    
+    //MARK: - TableView
+    private var mainTableView: UITableView!
+    
+    let activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        return activityIndicator
+    }()
+    
+    //MARK: - ViewLifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configureNavigationBar()
+        configureActivityIndicator()
+        
+        networkManager.fetchData(url: url, type: PostsModel.self) { posts in
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.posts = posts.posts
+                self.configureTableView()
+                self.activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    //MARK: - TableView
+    func configureTableView() {
+        mainTableView = UITableView()
+        mainTableView.separatorStyle = .none
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+        mainTableView.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.id)
+        mainTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(mainTableView)
+        
+        mainTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        mainTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        mainTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        mainTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    //MARK: - ActivityIndicator
+    private func configureActivityIndicator() {
+        view.addSubview(activityIndicator)
+        activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        activityIndicator.startAnimating()
+    }
+    
+    //MARK: - NavigationBar configuration
+    private func configureNavigationBar() {
+        navigationItem.title = "Natife"
+        
+        let items = UIMenu(title: "More", options: .displayInline, children: [
+            UIAction(title: "Sorting by date", image: UIImage(systemName: "calendar.badge.clock"), handler: { [unowned self] _ in
+                self.posts?.sort(by: { $0.timeshamp > $1.timeshamp })
+                self.mainTableView.reloadData()
+                self.scrollToTop()
+            }),
+            UIAction(title: "Sorting by likes", image: UIImage(systemName: "heart"), handler: { [unowned self] _ in
+                self.posts?.sort(by: { $0.likesCount > $1.likesCount })
+                self.mainTableView.reloadData()
+                self.scrollToTop()
+            }),
+            
+            UIAction(title: "Sorting by default", image: UIImage(systemName: "heart"), handler: { [unowned self] _ in
+                self.posts?.sort(by: { $0.postID > $1.postID})
+                self.mainTableView.reloadData()
+                self.scrollToTop()
+            }),
+            
+        ])
+        
+        let menu = UIMenu(title: "Select a sorting method", children: [items])
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "", image: UIImage(named: "filterbutton"), primaryAction: nil, menu: menu)
+        navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.2274509804, green: 0.3254901961, blue: 0.3647058824, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20) ,
+            NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.2274509804, green: 0.3254901961, blue: 0.3647058824, alpha: 1)]
+    }
+    
+    //MARK: - TableView Scroll To Top
+    private func scrollToTop() {
+        let topRow = IndexPath(row: 0, section: 0)
+        mainTableView.scrollToRow(at: topRow, at: .top, animated: true)
+    }
+}
+
+//MARK: - TableView Delegate & DataSource
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        posts?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.id)
+                as? CustomTableViewCell else { return UITableViewCell() }
+        
+        if let post = posts?[indexPath.row] {
+            cell.setupCell(with: post)
+        }
+        
+        cell.handleState = {
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailsViewController = DetailsViewController()
+        guard let id = posts?[indexPath.row].postID else { return }
+        let url = URL(string: "https://raw.githubusercontent.com/anton-natife/jsons/master/api/posts/\(id).json")
+        detailsViewController.singlePostUrl = url
+        navigationController?.pushViewController(detailsViewController, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
