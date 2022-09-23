18d5804233cfb021e527e9beedf717a572670b59
spartannik <dead5042@gmail.com>
2022-09-23 17:08:38 +0300

    fix

--- iOSTrainee/Launch/SceneDelegate.swift
+++ iOSTrainee/Launch/SceneDelegate.swift
@@ -12,9 +12,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let viewController = MainViewController()
+
+        let networkService = NetworkService()
+        let presenter = Presenter(networkService: networkService)
+
+        viewController.presenter = presenter
+        presenter.view = viewController
         let navigationController = UINavigationController(rootViewController: viewController)
+
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
